78f5b8b4d1622826dd141247e470d19c24b613aa
YenochQin <41689934+YenochQin@users.noreply.github.com>
2022-03-17 09:42:06 +0800

    Add files via upload

---
+++ level_compare.py
@@ -0,0 +1,246 @@
+#! python3
+# UTF-8
+# Qin yi
+# GRASP levels comparision
+#################################################
+"""
+    Version 1.0.0(11/1/2021)
+    first version for read levels from 
+    rlevels even.m > {atom}{as}
+    this version didn't consider different parities
+    
+    Version 1.0.1(11/2/2021)
+    this version could read levels from
+    rlevels {atom}{even}{as}.m {atom}{odd}{as}.m> {atom}{as}
+    and compare the difference between two active space(consider parities)
+    
+    Version 1.0.2(12/1/2021)
+    Add "Configuration" and "LSj" columns in DataFrame. 
+    Save DataFrame as csv files
+    
+    Version 1.0.3(12/2/2021)
+    Change level_storage function to level_mcdhf_storage.
+    Add level_add function to add CI calculation results.
+    level_read function could detect the Configurations in energy files, and add "Configuration
+    
+    Version 1.0.4(12/12/2021)
+    Add file.close()
+    
+    Version 1.0.5(12/17/2021)
+    Change level configuration storage strategy.
+    Due to the DHF results and the active space optimization results may have difference, the as0's configuration added.
+    Add configuration skip VALUE to omit some electron orbital which is not needed.
+    
+    Version 1.0.6(12/18/2021)
+    Add level's composition of ASF function.
+    
+    Version 1.0.7(1/10/2022)
+    Add "Configuration"   subshells LS-coupling state, for better identifying the atomic state.
+"""
+
+import numpy as np
+import pandas as pd
+import re
+
+# load level data from {atom}{as}
+
+def level_read(levelas, skipconf=0):
+    print(levelas)
+    
+    # This for strorange configuration' col_index
+    if 'CI' not in levelas:
+        findas = levelas.index('as')
+        thisas = levelas[findas: findas+3]
+    else:
+        findas = levelas.index('as')
+        thisas = 'CI' + levelas[findas: findas+3]
+    
+    level_file = open(levelas, 'r')
+    level_data = level_file.readlines()
+    level_file.close()
+    
+    # This for skip line to read levels 
+    # ' 1 ' is the target in the first level line
+    for i in range(len(level_data)):
+        if '  1  ' in level_data[i]:
+            skip_line = i
+            break
+    
+    # define 'level' to store the level data
+    level_as = pd.DataFrame(columns=['No', 'Pos', 'J', 'Parity', 'Levels',f'Configuration_{thisas}raw'])
+    
+    # store level data to 'level' array
+    for line in range(0,len(level_data)-skip_line-1):
+        # print(level_data[line+skip_line])
+        level_source = level_data[line+skip_line].split()
+        # print(level_source)
+        if len(level_source) != 8:
+            if line == 0 :
+                level_as.loc[line, ['No', 'Pos', 'J', 'Parity', 'Levels']] = [level_source[0], level_source[1], level_source[2], level_source[3], float(0)]
+            else:
+                level_as.loc[line, ['No', 'Pos', 'J', 'Parity', 'Levels']] =[level_source[0], level_source[1], level_source[2], level_source[3], level_source[5]]
+        else:
+            level_as.loc[line, ['No', 'Pos', 'J', 'Parity', 'Levels', f'Configuration_{thisas}raw']] =[level_source[0], level_source[1], level_source[2], level_source[3], level_source[5], level_source[7]]
+
+    # change Levels columns type to float
+    level_as['Levels'] = level_as.Levels.astype(np.float64)
+
+    # change Configuration into a LaTeX equation form
+    if not level_as[f'Configuration_{thisas}raw'].isnull().any():
+        # print(level_as['Configuration_raw'])
+        for n in range(len(level_as[f'Configuration_{thisas}raw'])):
+            conf_temp = level_as.loc[n, f'Configuration_{thisas}raw'][skipconf:].split('.')
+            temp_conf = ''
+            for subshell in conf_temp:
+                print(subshell)
+                if '(' and ')' in subshell:
+                    ele_num = re.findall(r"[(](.*?)[)]", subshell)
+                    temp_subshell_LS_transform = ''
+                    if re.findall(r"[)]([0-9][A-Z][0-9]?)[_]", subshell):
+                        temp_subshell_LS = re.findall(r"[)]([0-9][A-Z][0-9]?)[_]", subshell)
+                        temp_subshell_LS_transform = f"\\,(^{temp_subshell_LS[0][0]}_{temp_subshell_LS[0][-1]}{temp_subshell_LS[0][1]})"
+                    temp_conf= temp_conf + f'{subshell[0:2]}^{{{ele_num[0]}}}{temp_subshell_LS_transform}\\,' 
+                    print(temp_conf)
+                else:
+                    if subshell != conf_temp[-1]:
+                        temp_subshell_LS_transform = ''
+                        if '_' in subshell:
+                            temp_subshell_LS = re.findall(r"[_]([0-9][A-Z]?)", subshell)
+                            temp_subshell_LS_transform = f"\\,^{temp_subshell_LS[0][0]}{temp_subshell_LS[0][1]}"
+                        temp_conf = temp_conf + f'{subshell[0:2]}{temp_subshell_LS_transform}\\,'
+                    elif subshell == conf_temp[-1]:
+                        temp_conf = temp_conf + f'{subshell[0:2]}\\,'
+                        
+            ls = conf_temp[-1][-2:]
+
+            level_as.loc[n, f'Configuration_{thisas}']= f'${temp_conf }$'
+            j = level_as.loc[n, 'J']
+            if level_as.loc[n, 'Parity'] == '-':
+                level_as.loc[n, f'LSj_{thisas}'] = f'$\\,^{ls[0]}{ls[1]}_{{{j}}}^{{o}}$'
+            elif level_as.loc[n, 'Parity'] == '+':
+                level_as.loc[n, f'LSj_{thisas}'] = f'$\\,^{ls[0]}{ls[1]}_{{{j}}}$'
+    return level_as
+
+def level_mcdhf_storage(atom, dir, parameters, max_as=np.nan, min_as=0, skipconf=0):
+    for a in range(min_as, max_as+1):
+        level_temp = level_read(f'{dir}/{atom}{parameters}{a}', skipconf)
+        if a == 0:
+            level = level_temp.copy()
+            level.rename(columns={'Levels': f'{parameters}{a}'}, inplace = True)
+        else:
+            level[f'{parameters}{a}'] = np.nan
+            for i in range(0, len(level)):
+                for j in range(0, len(level)):
+                    if level.loc[i,'Pos'] == level_temp.loc[j,'Pos'] and level.loc[i,'J'] == level_temp.loc[j,'J'] and level.loc[i,'Parity'] == level_temp.loc[j,'Parity']:
+                        level.loc[i,f'{parameters}{a}'] = level_temp.loc[j, 'Levels']
+    level['No'] = level.No.astype(np.int32)
+    level['Comp_of_asf'] = ''
+    return level
+
+# add extra energy levels (like RCI results) to level DataFrame
+def level_add(level, atom, dir, parameters, a_s=np.nan, skipconf=0):
+    if 'CI' in parameters:
+        thisas = 'CIas' + str(a_s)
+    level_temp = level_read(f'{dir}/{atom}{parameters}{a_s}', skipconf)
+    level[f'{parameters}{a_s}'] = np.nan
+    for i in range(0, len(level)):
+        for j in range(0, len(level_temp)):
+            if level.loc[i,'Pos'] == level_temp.loc[j,'Pos'] and level.loc[i,'J'] == level_temp.loc[j,'J'] and level.loc[i,'Parity'] == level_temp.loc[j,'Parity']:
+                level.loc[i,f'{parameters}{a_s}'] = level_temp.loc[j, 'Levels']
+                level.loc[i, f'Configuration_{thisas}'] = level_temp.loc[j, f'Configuration_{thisas}']
+                level.loc[i, f'LSj_{thisas}'] = level_temp.loc[j, f'LSj_{thisas}']
+                level.loc[i, f'Configuration_{thisas}raw'] = level_temp.loc[j, f'Configuration_{thisas}raw']
+    return level   
+
+# difference between two as levels
+def level_diff(level,  parameters, max_as):
+    for a in range(max_as):
+        if a == 0:
+            level[f'dE{a+1}'] = level[f'{parameters}{a+1}'] - level[f'{parameters}{a}']
+            level[f'dE{a+1}per'] = (level[f'{parameters}{a+1}'] - level[f'{parameters}{a}']) / level[f'{parameters}{a}']
+        else:
+            level[f'dE{a+1}'] = level[f'{parameters}{a+1}'] - level[f'{parameters}{a}']
+            level[f'dE{a+1}per'] = (level[f'{parameters}{a+1}'] - level[f'{parameters}{a}']) / level[f'{parameters}{a}']
+    return level
+
+
+# Add level's composition of ASF
+def level_comp_asf(level, dir, parity, parameters, max_as, min_comp=0.03, skipconf=0):
+
+    lsjlblfile = open(f'{dir}/{parity}{parameters}{max_as}.lsj.lbl', 'r')
+    lsjlbl = lsjlblfile.readlines()
+    lsjlblfile.close()
+    # lvloclbl =level located in lbl file
+    lvloclbl = []
+
+    for line in range(0,len(lsjlbl)):
+        # print(lsjlbl[line])
+        if re.search('\d+.\d+%', lsjlbl[line]) :
+            lvloclbl.append(line)
+            # print(line)
+
+    compasf = []
+    lvloc = []
+    for i in range(len(lvloclbl)):
+        if lvloclbl[i] != lvloclbl[-1]:
+            lvloc.append(lsjlbl[lvloclbl[i]].split())
+            comp_temp=[]
+            for line in lsjlbl[lvloclbl[i]+1: lvloclbl[i+1]]:
+                if line != ' \n':
+                    comp_temp.append(line.split())
+        else:
+            lvloc.append(lsjlbl[lvloclbl[i]].split())
+            comp_temp=[]
+            for line in lsjlbl[lvloclbl[i]+1: ]:
+                # print(line)
+                if line != ' \n':
+                    comp_temp.append(line.split())
+        compasf.append(comp_temp)
+
+    for l in range(len(lvloc)):
+        lvloc_temp = lvloc[l]
+        lv_index = level.loc[(level['Pos'] == lvloc_temp[0]) & (level['J'] == lvloc_temp[1]) & (level['Parity']== lvloc_temp[2])].index[0]
+        
+        # Store the Comp of ASF in a temp DataFrame
+        comp_temp = pd.DataFrame(columns=['c', 'w', 'comp_conf'])
+        for line in range(len(compasf[l])):
+            comp_temp.loc[line, ['c', 'w', 'comp_conf']] = compasf[l][line]
+
+        comp_temp['w'] = comp_temp.w.astype(np.float64).round(2)
+        comp_temp = comp_temp[comp_temp['w'] >= min_comp]
+        # skipconf = 25
+        comp_temp['comp_conf'] = comp_temp['comp_conf'].str[skipconf:]
+        print(comp_temp)
+        # temp_tlevel = level.loc[lv_index, 'Configuration_as0raw']
+        
+        for n in range(len(comp_temp['comp_conf'])):
+            conf_temp = comp_temp.loc[n, 'comp_conf'].split('.')
+            temp_comp_conf = ''
+            temp_comp_of_asf  = ''
+            for subshell in conf_temp:
+                print(subshell)
+                if '(' and ')' in subshell:
+                    ele_num = re.findall(r"[(](.*?)[)]", subshell)
+                    temp_subshell_LS_transform = ''
+                    if re.findall(r"[)]([0-9][A-Z][0-9]?)[_]", subshell):
+                        temp_subshell_LS = re.findall(r"[)]([0-9][A-Z][0-9]?)[_]", subshell)
+                        temp_subshell_LS_transform = f"\\,(^{temp_subshell_LS[0][0]}_{temp_subshell_LS[0][-1]}{temp_subshell_LS[0][1]})"
+                    temp_comp_conf= temp_comp_conf + f'{subshell[0:2]}^{{{ele_num[0]}}}{temp_subshell_LS_transform}\\,' 
+                    print(temp_comp_conf)
+                else:
+                    if subshell != conf_temp[-1]:
+                        temp_subshell_LS_transform = ''
+                        if '_' in subshell:
+                            temp_subshell_LS = re.findall(r"[_]([0-9][A-Z]?)", subshell)
+                            temp_subshell_LS_transform = f"\\,^{temp_subshell_LS[0][0]}{temp_subshell_LS[0][1]}"
+                        temp_comp_conf = temp_comp_conf + f'{subshell[0:2]}{temp_subshell_LS_transform}\\,'
+                    elif subshell == conf_temp[-1]:
+                        temp_comp_conf = temp_comp_conf + f'{subshell[0:2]}\\,'
+                temp_comp_ls = conf_temp[-1][-2:]
+                comp_LS = f"^{temp_comp_ls[0]}{temp_comp_ls[1]}"
+            temp_comp_of_asf = temp_comp_of_asf+ '$' + str(comp_temp.loc[n,'w']) + '\\;'  + temp_comp_conf + '\\,' + comp_LS + '$ + '
+
+            level.loc[lv_index, 'Comp_of_asf'] = level.loc[lv_index, 'Comp_of_asf'] + temp_comp_of_asf
+        level.loc[lv_index, 'Comp_of_asf'] = level.loc[lv_index, 'Comp_of_asf'][:-2]
+
+    return level   
---
+++ transition_data2csv.py
@@ -0,0 +1,191 @@
+#! python3
+# UTF-8
+# Qin yi
+# GRASP transition data transfer to csv file
+
+"""
+    Version 1.0.0(11/23/2021)
+    first version for read transition data from *.ct and energylable.latex(produced by rtabtransition1)
+    
+    Version 1.1.0(12/24/2021)
+    Function is full finished.
+    The function only read data from *ct files and collect the data into one DataFrame. 
+    The function strore the Electric transition in Coulomb gauge. And it can calculate wavelength and uncertainty automatically.
+    The function also can search the index from the level DataFrame, and sort the Lower_index from smallest to biggest, so do the Upper_index.
+    
+    Version 1.1.1(1/11/2022)
+"""
+import os
+import numpy as np
+import pandas as pd
+import re
+
+
+
+def data_load(file):
+    # load*.ct file
+    transition_file = open(f'{file}','r')
+    transition_data = transition_file.readlines()
+    transition_file.close()
+    
+    transition_file_df = pd.DataFrame(columns=['Lower_index', 'Lower_conf', 'Lowerlev', 'Upper_index', 'Upper_conf','Upperlev', 'Type', 'deltaE', 'Wavelength', 'A_l', 'gf_l', 'S_l', 'A_v', 'gf_v', 'S_v', 'BR', 'deltaS'])
+    
+    loc_tran_data = []
+    """[summary]: This for loop find the location of the start transition data and store them in a list > loc_tran_data
+
+    Returns:
+        [type: list]: [description]: location and transition type in a list > loc_tran_data
+    """
+    for line in range(len(transition_data)):
+        if 'Electric' in transition_data[line]:
+            if line+5 >= len(transition_data):
+                break 
+            elif 'f' in transition_data[line+5]:
+                print(line)
+                temp_pole= re.findall(r'\(+ +[0-9]\)', transition_data[line]) 
+                print('E',temp_pole)
+                loc_tran_data.append(line)
+                loc_tran_data.append('E'+f"{temp_pole[0][2]}")
+        elif 'Magnetic' in transition_data[line]:
+            if line+5 >= len(transition_data):
+                break 
+            elif 'f' in transition_data[line+5]:
+                print(line)
+                temp_pole= re.findall(r'\(+ +[0-9]\)', transition_data[line]) 
+                print('M',temp_pole)
+                loc_tran_data.append(line)
+                loc_tran_data.append('M'+f"{temp_pole[0][2]}")
+                
+    """[summary] load and process the transition data into transtition_df DataFrame in sequence of list 'loc_tran_data'.
+
+    Returns:
+        [type: DataFrame]: [description]
+    """
+    for i in range(0,len(loc_tran_data),2):
+        print(loc_tran_data[i])
+        tran_line = loc_tran_data[i] + 5
+        if 'E' in loc_tran_data[i+1]:
+            tran_data_temp = pd.DataFrame(columns=['Upperlev', 'Lowerlev', 'Type', 'deltaE', 'A_l',  'gf_l', 'S_l', 'A_v', 'gf_v', 'S_v'])   # Electric *pole transition have two form: Babushkin gauge(length gauge) and Coulomb gauge(velocity gauge).
+            no_tran = 0
+            for tran_data_line in range(tran_line, len(transition_data)):
+                # print(transition_data[tran_data_line])
+                temp_data = transition_data[tran_data_line]
+                if 'f' in temp_data and 'C' in temp_data:
+                    # print(temp_data)
+                    temp_data = temp_data.split()
+                    tran_data_temp.loc[no_tran,['Upperlev', 'Lowerlev', 'Type', 'deltaE', 'A_v', 'gf_v', 'S_v']] = [" ".join(temp_data[0:4]), " ".join(temp_data[4:8]), f"{loc_tran_data[i+1]}", temp_data[8], temp_data[10], temp_data[11], temp_data[12] ] 
+
+                elif 'B' in temp_data:
+                    temp_data = temp_data.split()
+                    tran_data_temp.loc[no_tran,['A_l', 'gf_l', 'S_l']] = [temp_data[1], temp_data[2], temp_data[3] ]
+                    no_tran += 1
+                elif '\n' == temp_data:
+                    break
+
+                tran_data_temp.replace(r'(.*)D(.*)', r'\1E\2',regex=True, inplace=True)
+                tran_data_temp[['deltaE','A_v', 'A_l', 'gf_v', 'gf_l', 'S_v', 'S_l']] = tran_data_temp[['deltaE', 'A_v', 'A_l', 'gf_v', 'gf_l', 'S_v', 'S_l']].astype(np.float64, errors = 'raise')
+                tran_data_temp = tran_data_temp[(tran_data_temp.S_v != 0) & (tran_data_temp.S_l != 0)]
+                tran_data_temp['deltaS'] = tran_data_temp.apply( lambda x :  abs(x['S_v'] - x['S_l']) / max(x['S_v'], x['S_l']), axis=1)
+                # exec( f"{loc_tran_data[i+1]}_temp = tran_data_temp")
+            # tran_data_temp.rename(columns={'A_l' : 'A', 'gf_l' : 'gf', 'S_l' : 'S' }, inplace=True)
+            transition_file_df = transition_file_df.append(pd.DataFrame(tran_data_temp, columns=['Upperlev', 'Lowerlev', 'Type' ,'deltaE','A_v', 'A_l', 'gf_v', 'gf_l', 'S_v', 'S_l', 'deltaS']), ignore_index=True)
+            
+        elif 'M' in loc_tran_data[i+1]:
+            tran_data_temp = pd.DataFrame(columns=['Upperlev', 'Lowerlev', 'Type', 'deltaE', 'A_l',  'gf_l', 'S_l', 'A_v', 'gf_v', 'S_v'])   
+            tran_data_temp.deltaS = np.nan
+            no_tran = 0
+            for tran_data_line in range(tran_line, len(transition_data)):
+                temp_data = transition_data[tran_data_line]
+                # print(temp_data)
+                if 'M' in temp_data:
+                    temp_data = temp_data.split()
+                    tran_data_temp.loc[no_tran,['Upperlev', 'Lowerlev','Type', 'deltaE', 'A_l', 'gf_l', 'S_l']] = [" ".join(temp_data[0:4]), " ".join(temp_data[4:8]), f"{loc_tran_data[i+1]}", temp_data[8], temp_data[10], temp_data[11], temp_data[12]] 
+                    tran_data_temp.loc[no_tran,['Upperlev', 'Lowerlev','Type', 'deltaE', 'A_v', 'gf_v', 'S_v']] = [" ".join(temp_data[0:4]), " ".join(temp_data[4:8]), f"{loc_tran_data[i+1]}", temp_data[8], temp_data[10], temp_data[11], temp_data[12]] 
+                    no_tran += 1
+                elif '\n' == temp_data:
+                    break
+                tran_data_temp.replace(r'(.*)D(.*)', r'\1E\2',regex=True, inplace=True)
+                tran_data_temp[['deltaE', 'A_l',  'gf_l', 'S_l', 'A_v', 'gf_v', 'S_v']] = tran_data_temp[['deltaE', 'A_l',  'gf_l', 'S_l', 'A_v', 'gf_v', 'S_v']].astype(np.float64, errors = 'raise')
+                # exec( f"{loc_tran_data[i+1]}_temp = tran_data_temp")
+                tran_data_temp = tran_data_temp[(tran_data_temp.A_l != 0)&(tran_data_temp.gf_l != 0)]
+            transition_file_df = transition_file_df.append(pd.DataFrame(tran_data_temp, columns=['Upperlev', 'Lowerlev', 'Type' ,'deltaE',  'A_l',  'gf_l', 'S_l', 'A_v', 'gf_v', 'S_v']), ignore_index=True)
+            
+    return transition_file_df
+
+def data_process(transition_df, level):
+    """[summary]: Find the index of lower and upper levels in level_DataFrame. Add the results into transition_df and sort the Lower_index from smallest to biggest, so do the Upper_index.
+
+    Args:
+        transition_df ([type: DataFrame]): [description] : columns=['Lower_index', 'Lower_conf', 'Lowerlev', 'Upper_index', 'Upper_conf','Upperlev', 'Type', 'deltaE', 'Wavelength', 'A', 'gf', 'S', 'deltaS']
+
+    Returns:
+        [type : DataFrame]: [description: return transition_df]
+    """
+    for line in range(len(transition_df)):
+    #     print(transition_df.loc[line])
+        Up_temp = transition_df.loc[line,'Upperlev'].split()
+        Low_temp = transition_df.loc[line,'Lowerlev'].split()
+        Up_index = level[(level.Pos == Up_temp[1]) & (level.J == Up_temp[2]) & (level.Parity == Up_temp[3])].index.tolist()[0]
+        Low_index = level[(level.Pos == Low_temp[1]) & (level.J == Low_temp[2]) & (level.Parity == Low_temp[3])].index.tolist()[0]
+                # print(Upper_index)
+                # print(Lower_index)
+        transition_df.loc[line, ['Upper_index', 'Upper_conf', 'Lower_index', 'Lower_conf']] =[level.loc[Up_index, 'No'], level.loc[Up_index, 'Configuration_CIas5raw'], level.loc[Low_index, 'No'], level.loc[Low_index, 'Configuration_CIas5raw']]
+        transition_df.loc[line, 'Wavelength'] = 10 ** 8 / transition_df.loc[line, 'deltaE'] 
+        
+        
+    transition_df['Lower_index'] = transition_df.Lower_index.astype(np.int8)
+    transition_df['Upper_index'] = transition_df.Upper_index.astype(np.int8)
+    transition_df.sort_values(by=['Lower_index', 'Upper_index'], ascending=True, inplace=True)
+    level['lifetime_l'] = 0
+    level['lifetime_l'] = level.lifetime_l.astype(np.float64)
+    level['lifetime_v'] = 0
+    level['lifetime_v'] = level.lifetime_v.astype(np.float64)
+    # level.loc[0,'lifetime'] = 0
+    for lno in range(2, len(level)+1, 1):
+        temp_sum_A_l = transition_df.loc[(transition_df['Upper_index'] == lno), 'A_l'].sum()
+        temp_sum_A_v = transition_df.loc[(transition_df['Upper_index'] == lno), 'A_v'].sum()
+        print(temp_sum_A_l,temp_sum_A_v)
+        if temp_sum_A_l != 0:
+            transition_df.loc[(transition_df['Upper_index'] == lno), 'sum_A_l'] = temp_sum_A_l
+            temp_lifetime_l = 1 / temp_sum_A_l
+            level.loc[lno-1, 'lifetime_l'] = temp_lifetime_l
+        else:
+            continue
+        if temp_sum_A_v != 0:
+            transition_df.loc[(transition_df['Upper_index'] == lno), 'sum_A_v'] = temp_sum_A_v
+            temp_lifetime_v = 1 / temp_sum_A_v
+            level.loc[lno-1, 'lifetime_v'] = temp_lifetime_v
+        else:
+            continue
+    transition_df['BR'] = np.float64(0)
+    for trannum in range(len(transition_df)):
+        transition_df.loc[trannum, 'BR'] = transition_df.loc[trannum, 'A_l']/transition_df.loc[trannum, 'sum_A_l']
+        if transition_df.loc[trannum, 'BR'] <= 0.0001:
+            transition_df.drop(trannum, axis=0, inplace=True)
+    
+    return transition_df, level
+
+
+
+def data_collect(dir):
+    """
+    [summary]: collect all transition data at once.
+    """
+    dir_temp= os.listdir(dir)
+    trans_seque = []
+    for i in dir_temp:
+        if 'even' in i or 'odd' in i and '-' in i:
+            trans_seque.append(i)
+    
+    for s in trans_seque:
+        trans_sq_file = os.listdir(f'{dir}/{s}')
+        for f in trans_sq_file:
+            if f.split('.')[-1] == 'ct':
+                print(f)
+                file = f'{dir}/{s}/{f}'
+                trans_data = data_load(file)
+                if 'transition_df' not in locals().keys():
+                    transition_df = trans_data.copy()
+                else:
+                    transition_df = transition_df.append(pd.DataFrame(trans_data), ignore_index=True)
+    return transition_df
